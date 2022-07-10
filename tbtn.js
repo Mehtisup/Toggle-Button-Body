11696ec576001343ac921e03d8f71950077ae889
Mehti <102772767+Mehtisup@users.noreply.github.com>
2022-07-11 02:14:31 +0430

    Create tbtn.js

---
+++ tbtn.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>$UP</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="red">
+<link rel="icon" type="image/x-icon" href="">
+<style>
+.a {
+    background-color: black;
+}
+</style>
+</head>
+<body>
+<button type="button" class="btn" onclick="myFunction()">CLICK</button>
+
+<script>
+function myFunction() {
+    var x = document.body;
+    x.classList.toggle("a")
+}
+</script>
+
+</body>
+</html>
